8bea0b1a25fedd4e1c5cec9ee07661c37b7a38d8
Jay <jay.lligui@gmail.com>
2018-10-01 20:08:24 -0400

    Beta website
    learned bootstrap 4 all day yesterday, implementing it now

--- myJavaScript.js
+++
@@ -1,9 +0,0 @@
-// function buttonClick() {
-//   document.getElementById("button1").onclick = function () {
-//        location.href = "mainPage.html";
-//    };
-// }
-
-var shiftWindow = function() { scrollBy(0, -72) };
-if (location.hash) shiftWindow();
-window.addEventListener("hashchange", shiftWindow);
